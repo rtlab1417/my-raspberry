4a3c290395fd864f5ea98f2a1b7fb7194e90604f
rtlab1417 <65482062+rtlab1417@users.noreply.github.com>
2020-09-12 10:35:36 +0800

    Add files via upload

---
+++ LED1.py
@@ -0,0 +1,11 @@
+# https://thepihut.com/blogs/raspberry-pi-tutorials/27968772-turning-on-an-led-with-your-raspberry-pis-gpio-pins
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print("LED on")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print("LED off")
+GPIO.output(18,GPIO.LOW)
